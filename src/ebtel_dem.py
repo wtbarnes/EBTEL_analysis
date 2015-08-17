a01a9a0a6920d1ca21991d2640e5732b315908e5
Will Barnes <wtb2@rice.edu>
2015-08-17 18:54:52 -0500

    adding residual factor to error estimation

--- src/ebtel_dem.py
+++ src/ebtel_dem.py
@@ -239,7 +239,9 @@ class DEMAnalyze(object):
         else:
             pars_cool,covar_cool = curve_fit(linear_fit,bound_arrays['temp_cool'],bound_arrays['dem_cool'],sigma=bound_arrays['sigma_cool'],absolute_sigma=True)
             a_coolward,b_coolward = pars_cool[0],pars_cool[1]
-            sigma_coolward = np.sqrt(np.diag(covar_cool))
+            #compute residual variance
+            res = np.sum((linear_fit(bound_arrays['temp_cool'],*pars_cool) - bound_arrays['dem_cool'])**2)/(len(bound_arrays['dem_cool']) - len(pars_cool))
+            sigma_coolward = np.sqrt(np.diag(covar_cool*res))
             
         #hot
         if bound_arrays['temp_hot'] is False:
@@ -249,7 +251,8 @@ class DEMAnalyze(object):
         else:
             pars_hot,covar_hot = curve_fit(linear_fit,bound_arrays['temp_hot'],bound_arrays['dem_hot'],sigma=bound_arrays['sigma_hot'],absolute_sigma=True)
             a_hotward,b_hotward = pars_hot[0],pars_hot[1]
-            sigma_hotward = np.sqrt(np.diag(covar_hot))
+            res = np.sum((linear_fit(bound_arrays['temp_hot'],*pars_hot) - bound_arrays['dem_hot'])**2)/(len(bound_arrays['dem_hot']) - len(pars_hot))
+            sigma_hotward = np.sqrt(np.diag(covar_hot*res))
             
         return a_coolward,b_coolward,sigma_coolward,a_hotward,b_hotward,sigma_hotward
         
