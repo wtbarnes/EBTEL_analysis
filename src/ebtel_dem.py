15ea7dd422f0cbdc05f03e9b262464aef730b076
Will Barnes <wtb2@rice.edu>
2015-09-16 18:16:49 -0500

    applying uniform weighting of 1 to fitting procedure to test how this affects fit parameter uncertainties

--- src/ebtel_dem.py
+++ src/ebtel_dem.py
@@ -237,7 +237,7 @@ class DEMAnalyze(object):
             b_coolward = False
             sigma_coolward = False
         else:
-            pars_cool,covar_cool = curve_fit(linear_fit,bound_arrays['temp_cool'],bound_arrays['dem_cool'],sigma=bound_arrays['sigma_cool'],absolute_sigma=True)
+            pars_cool,covar_cool = curve_fit(linear_fit,bound_arrays['temp_cool'],bound_arrays['dem_cool'])#,sigma=bound_arrays['sigma_cool'],absolute_sigma=True)
             a_coolward,b_coolward = pars_cool[0],pars_cool[1]
             #compute residual variance
             res = 1#np.sum((linear_fit(bound_arrays['temp_cool'],*pars_cool) - bound_arrays['dem_cool'])**2)/(len(bound_arrays['dem_cool']) - len(pars_cool))
@@ -249,7 +249,7 @@ class DEMAnalyze(object):
             b_hotward = False
             sigma_hotward = False
         else:
-            pars_hot,covar_hot = curve_fit(linear_fit,bound_arrays['temp_hot'],bound_arrays['dem_hot'],sigma=bound_arrays['sigma_hot'],absolute_sigma=True)
+            pars_hot,covar_hot = curve_fit(linear_fit,bound_arrays['temp_hot'],bound_arrays['dem_hot'])#,sigma=bound_arrays['sigma_hot'],absolute_sigma=True)
             a_hotward,b_hotward = pars_hot[0],pars_hot[1]
             res = 1#np.sum((linear_fit(bound_arrays['temp_hot'],*pars_hot) - bound_arrays['dem_hot'])**2)/(len(bound_arrays['dem_hot']) - len(pars_hot))
             sigma_hotward = np.sqrt(np.diag(covar_hot/res))
