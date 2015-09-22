bug fixes to hot slope calculation

src/ebtel_dem.py
@@ -356,7 +356,7 @@ class DEMAnalyze(object):
             #calculate derivative
             dEMdT = np.gradient(em_hot,np.gradient(temp_hot))
             #set upper and lower temperature bounds
-            th_upper = temp[np.where(np.fabs(dEMdT)>2.0)[0][0]-1]
+            th_upper = temp[i_hot[np.where(np.fabs(dEMdT)>2.0)[0]][0]-1]
             th_lower = th_upper - self.delta_t
             tc_lower = self.slope_limits['cool_lower']
             tc_upper = self.slope_limits['cool_upper']