0a75a6eff90d69ff5719d057f565c6a2ff9b6e86
Will Barnes <wtb2@rice.edu>
2015-09-21 23:57:30 -0500

    hot slope calc debug

--- src/ebtel_dem.py
+++ src/ebtel_dem.py
@@ -354,7 +354,7 @@ class DEMAnalyze(object):
             em_hot = em[i_hot]
             temp_hot = temp[i_hot]
             #set upper and lower temperature bounds for 80% drop in EM from peak
-            th_upper = temp[i_hot[np.where(em_hot<0.95*np.max(em))[0]][0]-1]
+            th_upper = temp[i_hot[np.where(em_hot<0.90*np.max(em))[0]][0]-1]
             th_lower = th_upper - self.delta_t
             tc_lower = self.slope_limits['cool_lower']
             tc_upper = self.slope_limits['cool_upper']
