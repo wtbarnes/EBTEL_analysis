359832495171cc68b11fa8d178de0d738dde1ade
Will Barnes <wtb2@rice.edu>
2015-09-24 03:13:43 -0500

    styling

--- src/ebtel_plot_em.py
+++ src/ebtel_plot_em.py
@@ -297,7 +297,7 @@ class EMHistoBuilder(object):
         #Loop over histograms
         for key in hist_dict:
             #multiplier for uniform case
-            if key is 'uniform ':
+            if len(hist_dict[key]) =<20:
                 hist_dict[key] = 100*hist_dict[key]
             ax.hist(hist_dict[key], self.freedman_diaconis(hist_dict[key]), histtype='step',**kwargs['histo_opts'][key])# color=kwargs['histo_opts'][key]['color'], linestyle = kwargs['histo_opts'][key]['style'], label=kwargs['histo_opts'][key]['label'])
             
