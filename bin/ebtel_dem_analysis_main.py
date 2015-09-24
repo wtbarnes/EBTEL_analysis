282388474fdb8d0b14ec8f5b43fa1b7a20ab7c82
Will Barnes <wtb2@rice.edu>
2015-09-24 02:41:16 -0500

    abs val of hot slopes; removing clutter

--- bin/ebtel_dem_analysis_main.py
+++ bin/ebtel_dem_analysis_main.py
@@ -108,18 +108,3 @@ if args.alpha is not 'uniform':
     for k in range(len(t_wait)):
         plotter.plot_em_curve(k, print_fig_filename=root_dir_figs + fn_temp + '_dem_mc/' + figname%(args.loop_length,args.tpulse,args.alpha,args.species) + '_%d_dem'%k)
         
-        
-       # if 'fits_file' in kwargs:
-       #     #Write all hot and cool slopes to file using pandas/numpy
-       #     temp = self.cool_fits_all
-       #     for i in range(len(temp)):
-       #         for j in range(len(temp[i])):
-       #             if temp[i][j] is False:
-       #                 temp[i][j] = np.float('NaN')
-       #     np.savetxt(kwargs['fits_file']+'.cool',np.array(pd.DataFrame(temp)))
-       #     temp = self.hot_fits_all
-       #     for i in range(len(temp)):
-       #         for j in range(len(temp[i])):
-       #             if temp[i][j] is False:
-       #                 temp[i][j] = np.float('NaN')
-       #     np.savetxt(kwargs['fits_file']+'.hot',np.array(pd.DataFrame(temp)))
