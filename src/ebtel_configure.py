7015ea366e7270ca3180f656568799cd6ce32a64
Will Barnes <wtb2@rice.edu>
2016-02-03 15:42:26 -0600

    slimming down configuration code, fixing constraint on energies from power-law distribution to no longer use mean approach

--- src/ebtel_configure.py
+++ src/ebtel_configure.py
@@ -6,49 +6,44 @@
 #Import needed modules
 import numpy as np
 import os
+import logging
 import itertools
+import xml.etree.ElementTree as ET
+import xml.dom.minidom as xdm
 
 class Configurer(object):
     
-    def __init__(self,config_dictionary,root_dir,**kwargs):
+    def __init__(self, config_dictionary, root_dir, mc=None, t_wait_q_scaling = [], build_paths=False, Hn=None, delta_q=None, **kwargs):
+        """Constructor for Configurer class used to print EBTEL configuration files."""
+        
         self.config_dictionary = config_dictionary
         self.root_dir = root_dir
-        
-        #set optional heating variables
-        if 'Hn' in kwargs:
-            self.Hn = kwargs['Hn']
-            
-        if 'delta_q' in kwargs:
-            self.delta_q = kwargs['delta_q']
-            
-        if 't_wait_q_scaling' in kwargs:
-            self.t_wait_q_scaling = kwargs['t_wait_q_scaling']
-        else:
-            self.t_wait_q_scaling = []
-            
-        #check if we are using a Monte-Carlo Approach
         self.nmc_list = []
-        if 'mc' in kwargs:
-            #mc specifies the total number of events per Tn value
-            self.mc = kwargs['mc']
-        else:
-            self.mc = False
+                        
+        #set optional variables
+        self.Hn = Hn
+        self.delta_q = delta_q
+        self.mc = mc
+        self.t_wait_q_scaling = t_wait_q_scaling
+        
+        #configure logger
+        self.logger = logging.getLogger(type(self).__name__)
             
         #Set up paths
-        if 'build_paths' in kwargs and kwargs['build_paths'] is True:
+        if build_paths:
             self.path_builder()
         else:
-            print("Warning: Build paths not constructed! You will not be able to build config files for variable Tn until you run self.path_builder().")
+            self.logger.warning("Build paths not constructed! You will not be able to build config files for variable Tn until you run self.path_builder().")
 
 
     def path_builder(self,**kwargs):
         """Build path names needed for printing config files and building data folders; check that needed directories exist and create them if necessary."""
         
-        gen_path = self.config_dictionary['heat_species']+'_heating_runs/'            
+        gen_path = self.config_dictionary['heat_species']+'_heating_runs'            
         if self.config_dictionary['amp_switch'] == 'uniform':
-            gen_path = gen_path + 'alpha' + self.config_dictionary['amp_switch'] + '/'
+            gen_path = os.path.join(gen_path,'alpha' + self.config_dictionary['amp_switch'])
         else:
-            gen_path = gen_path + 'alpha' + str(np.fabs(self.config_dictionary['alpha'])) + '/'
+            gen_path = os.path.join(gen_path,'alpha' + str(np.fabs(self.config_dictionary['alpha']))) 
             
         #check for t_wait-q scaling
         if self.t_wait_q_scaling:
@@ -57,65 +52,42 @@ class Configurer(object):
             scaling_suffix = ''
         
         #set data and config paths
-        self.data_path = self.root_dir + gen_path + 'data/'
-        self.config_path = self.root_dir + gen_path + 'config/'
+        self.data_path = os.path.join(os.path.join(self.root_dir, gen_path),'data')
+        self.config_path = os.path.join(os.path.join(self.root_dir, gen_path),'config')
         self.fn = 'ebtel_L' + str(self.config_dictionary['loop_length']) + '_tn%d' + scaling_suffix + '_tpulse' + str(2.0*self.config_dictionary['t_pulse_half']) + '_' + self.config_dictionary['solver']
 
 
-    def print_xml_config(self,**kwargs):
+    def print_xml_config(self,config_file='../config/ebtel_config.xml',**kwargs):
         """Print EBTEL XML configuration file from dictionary of input parameters"""
-        
-        #Check if we have passed a filename
-        #If not, pass a default filename
-        if 'config_file' in kwargs:
-            config_file = kwargs['config_file']
-        else:
-            config_file = '../config/ebtel_config.xml'
-
-        #Open the file
-        f = open(config_file,'w')
-        #Print necessary header info
-        f.write('<?xml version="1.0" ?>\n')
-        f.write('<input>\n')
+            
+        #create root element
+        root = ET.Element('root')
 
         #Loop through dictionary and print to xml file
         for key in self.config_dictionary:
-            #Print tab delimiter, brackets and keyword
-            f.write('\t<')
-            f.write(key)
-            f.write('>')
-            #Check if entry is a list
-            #If so print it as a list
-            if isinstance(self.config_dictionary[key],list) or type(self.config_dictionary[key]).__name__ == 'ndarray':
-                #Make temporary list
-                temp = self.config_dictionary[key]
-                #Skip to new line
-                f.write('\n')
-                #Begin loop over list
-                for i in range(len(self.config_dictionary[key])):
-                    f.write('\t\t<')
-                    f.write(key+str(i))
-                    f.write('>')
-                    f.write(str(temp[i]))
-                    f.write('</')
-                    f.write(key+str(i))
-                    f.write('>\n')
-                    
-                #Print additional tab to preserve alignment
-                f.write('\t')
+            if (type(self.config_dictionary[key]) is not np.ndarray) and (type(self.config_dictionary[key]) is not list):
+                self._set_xml_element(root,key,self.config_dictionary[key])
             else:
-                #Print value
-                f.write(str(self.config_dictionary[key]))
+                element = ET.SubElement(root,key)
+                for i in range(len(self.config_dictionary[key])):
+                    self._set_xml_element(element,key+str(i),self.config_dictionary[key][i])
+        
+        #print to file
+        with open(config_file,'w') as f:
+            f.write(self._pretty_print_xml(root))
                 
-            #Close the brackets and print newline
-            f.write('</')
-            f.write(key)
-            f.write('>\n')
-
-        #Close the main node of the file
-        f.write('</input>')
-        #Close the file
-        f.close()
+                
+    def _set_xml_element(self,root,name,val):
+        """create element, set tag"""
+        element = ET.SubElement(root,name)
+        element.text = str(val)
+        
+    
+    def _pretty_print_xml(self,element):
+        """nicely formatted printing of XML files"""
+        unformatted = ET.tostring(element)
+        xdmparse = xdm.parseString(unformatted)
+        return xdmparse.toprettyxml(indent="    ")
         
         
     def print_job_array_config(self,**kwargs):
@@ -125,15 +97,14 @@ class Configurer(object):
             top_list = []
             for i in range(len(self.t_wait_mean)):
                 sub_list = []
-                for j in range(self.nmc_list[i]):
-                    sub_list.append([self.t_wait_mean[i],j])
-                    
+                [sub_list.append([self.t_wait_mean[i],j]) for j in range(self.nmc_list[i])]  
                 top_list.append(sub_list)
                 
             top_list_flattened = list(itertools.chain(*top_list))
-            np.savetxt(self.config_path+'ebtel_L' + str(self.config_dictionary['loop_length']) + '_tpulse' + str(2.0*self.config_dictionary['t_pulse_half']) + '_' + self.config_dictionary['solver'] + '_job_array.conf',top_list_flattened,fmt='%d')
-        except:
-            raise ValueError("Before printing the job_array.conf file, set up the config path with path_builder and then build the t_wait and nmc_list variables by running vary_wait_time.")
+            np.savetxt(os.path.join(self.config_path,'ebtel_L' + str(self.config_dictionary['loop_length']) + '_tpulse' + str(2.0*self.config_dictionary['t_pulse_half']) + '_' + self.config_dictionary['solver'] + '_job_array.conf'),top_list_flattened,fmt='%d')
+            
+        except AttributeError:
+            self.logger.exception("Before printing the job_array.conf file, set up the config path with path_builder and then build the t_wait and nmc_list variables by running self.vary_wait_time.")
 
 
     def vary_wait_time(self,tn_a,tn_b,delta_tn,**kwargs):
@@ -146,16 +117,16 @@ class Configurer(object):
             #set total number of events
             self.config_dictionary['num_events'] = int(np.ceil(self.config_dictionary['total_time']/(2.0*self.config_dictionary['t_pulse_half'] + self.t_wait_mean[i])))
             #Create directories in data and config if needed
-            if not os.path.exists(self.config_path+self.fn%self.t_wait_mean[i]):
-                os.makedirs(self.config_path+self.fn%self.t_wait_mean[i])
+            if not os.path.exists(os.path.join(self.config_path,self.fn%self.t_wait_mean[i])):
+                os.makedirs(os.path.join(self.config_path,self.fn%self.t_wait_mean[i]))
                 
-            if not os.path.exists(self.data_path+self.fn%self.t_wait_mean[i]):
-                os.makedirs(self.data_path+self.fn%self.t_wait_mean[i])
+            if not os.path.exists(os.path.join(self.data_path,self.fn%self.t_wait_mean[i])):
+                os.makedirs(os.path.join(self.data_path,self.fn%self.t_wait_mean[i]))
                 
             #Print config files for each run
             #Check if Monte-Carlo run
-            if self.mc is not False:
-                num_runs = self.calc_nmc()
+            if self.mc:
+                num_runs = self._calc_nmc()
             else:
                 num_runs = 1
                 
@@ -167,12 +138,12 @@ class Configurer(object):
                 #build start and end time arrays
                 self.time_arrays(self.t_wait_mean[i])
                 #set name of output file
-                self.config_dictionary['output_file'] = self.data_path+self.fn%self.t_wait_mean[i]+'/'+self.fn%self.t_wait_mean[i]+'_'+str(j)
+                self.config_dictionary['output_file'] = os.path.join(os.path.join(self.data_path+self.fn%self.t_wait_mean[i]), self.fn%self.t_wait_mean[i]+'_'+str(j))
                 #print configuration files
-                self.print_xml_config(config_file=self.config_path + self.fn%self.t_wait_mean[i] + '/' + self.fn%self.t_wait_mean[i] + '_' + str(j) + '.xml')
+                self.print_xml_config(config_file=os.path.join(os.path.join(self.config_path,self.fn%self.t_wait_mean[i]), self.fn%self.t_wait_mean[i] + '_' + str(j) + '.xml'))
                 
                 
-    def calc_nmc(self,**kwargs):
+    def _calc_nmc(self,**kwargs):
         """Calculate number of runs needed to maintain sufficiently large number of heating events"""
         
         return int(np.ceil(self.mc/self.config_dictionary['num_events']))
@@ -181,7 +152,10 @@ class Configurer(object):
     def time_arrays(self,ti,**kwargs):
         """Create start time and end time arrays"""
         
-        self.config_dictionary['start_time_array'],self.config_dictionary['end_time_array'] = np.empty([self.config_dictionary['num_events']]),np.empty([self.config_dictionary['num_events']])
+        #preallocate space for start, end time arrays
+        self.config_dictionary['start_time_array'], self.config_dictionary['end_time_array'] = np.empty([self.config_dictionary['num_events']]), np.empty([self.config_dictionary['num_events']])
+        #calculate coefficient (xi^(1/b))
+        xi_1ob = (self.config_dictionary['amp_array']**(1.0/self.t_wait_q_scaling)).sum()/(self.config_dictionary['total_time'] - self.config_dictionary['num_events']*2.0*self.config_dictionary['t_pulse_half'])
         #initialize running wait time sum
         t_wait_sum = 0.0
         #configure start and end time for each event
@@ -193,11 +167,7 @@ class Configurer(object):
             #calculate wait time sum
             if self.t_wait_q_scaling:
                 #calculate start time for wait time scaled to amplitude (Q=xi*T_N^b)
-                #calculate coefficient (xi^(1/b))
-                xi_1ob = (self.config_dictionary['amp_array']**(1.0/self.t_wait_q_scaling)).sum()/(self.config_dictionary['total_time'] - self.config_dictionary['num_events']*2.0*self.config_dictionary['t_pulse_half'])
-                #calculate resulting wait time
                 t_wait_temp = (self.config_dictionary['amp_array'][i])**(1.0/self.t_wait_q_scaling)/xi_1ob
-                #increment start time sum
                 t_wait_sum = t_wait_sum + t_wait_temp
             else:
                 #increment start time sum
@@ -207,30 +177,6 @@ class Configurer(object):
     def amp_arrays(self,ti,**kwargs):
         """Configure heating rate amplitudes"""
         
-        #calculate uniform heating amplitude
-        self.config_dictionary['h_nano'] = 2.0*self.Hn*self.config_dictionary['total_time']/(self.config_dictionary['num_events']*2.0*self.config_dictionary['t_pulse_half'])
-        #configure power-law bounds for given heating rate to maintain specified peak temperature
-        self.config_dictionary['amp0'], self.config_dictionary['amp1'] = self.distribution_bounds(ti)
-        #configure arrays of heating amplitudes from power_law distribution
-        if self.config_dictionary['amp_switch'] == 'file':    
-            np.random.seed()
-            x = np.random.rand(self.config_dictionary['num_events'])
-            self.config_dictionary['amp_array'] = self.power_law_dist(x)
-            
-            
-    def distribution_bounds(self,ti,**kwargs):
-        """Adjust bounds on power-law distribution such that the time-averaged heating rate is preserved with varying waiting time, Tn"""
-        
-        #compute coefficient based on alpha value
-        if self.config_dictionary['alpha'] < -2.0:
-            coeff =  (self.config_dictionary['alpha'] + 2.0)/(self.config_dictionary['alpha'] + 1.0)*(self.delta_q**(self.config_dictionary['alpha'] + 1.0) - 1.0)/(self.delta_q**(self.config_dictionary['alpha'] + 2.0) - 1.0)
-        elif self.config_dictionary['alpha'] == -2.0:
-            numer = self.delta_q**(self.config_dictionary['alpha']+1.0)*(1.0 + np.log(self.delta_q)*(self.config_dictionary['alpha'] + 2.0)) - 1.0
-            denom = self.delta_q**(self.config_dictionary['alpha']+2.0)*(1.0 + np.log(self.delta_q)*(self.config_dictionary['alpha'] + 1.0)) - 1.0
-            coeff =  numer/denom
-        else:
-            coeff =  3.0/((self.delta_q - 1.0))
-            
         #calculate coefficient to convert from energy to heating rate
         if self.config_dictionary['heating_shape'] == 'triangle':
             shape_coeff = 2.0/(2.0*self.config_dictionary['t_pulse_half'])
@@ -240,17 +186,51 @@ class Configurer(object):
             shape_coeff = 1.0/(self.config_dictionary['t_pulse_half']*np.sqrt(2.0*np.pi))
         else:
             raise ValueError("Unrecognized heating_shape option. Cannot set heating bounds.")
+            
+        #calculate uniform heating amplitude
+        self.config_dictionary['h_nano'] = self.Hn*self.config_dictionary['total_time']/(self.config_dictionary['num_events'])*shape_coeff
+        #dummy power-law bounds
+        self.config_dictionary['amp0'], self.config_dictionary['amp1'] = 0.001,0.1
+        if self.config_dictionary['amp_switch'] == 'power_law':
+            self.logger.warning("Setting dummy values for power-law bounds.")
+        #configure arrays of heating amplitudes from power-law distribution
+        if self.config_dictionary['amp_switch'] == 'file':    
+            np.random.seed()
+            self._constrain_distribution(ti)
+            
+            
+    def _constrain_distribution(self,ti,**kwargs):
+        """Choose events from power-law distribution such that total desired energy input is conserved."""
         
-        #calculate limits    
-        a0 = coeff*self.Hn*(ti + 2.0*self.config_dictionary['t_pulse_half'])*shape_coeff
-        a1 = self.delta_q*a0
-        
-        #return limits
-        return a0,a1
+        #set parameters
+        tol = 1e-2
+        max_tries = 1000
+        tries = 0
+        err = 1.e+300
+        #initial guess of bounds
+        a0 = 0.5*self.config_dictionary['h_nano']
+        a1 = self.delta_q*self.config_dictionary['amp0']
+        #begin iteration
+        while tries < max_tries and err > tol:
+            x = np.random.rand(self.config_dictionary['num_events'])
+            h = self._power_law_dist(x,a0,a1,self.config_dictionary['alpha'])
+            pl_sum = np.sum(h)
+            chi = self.config_dictionary['h_nano']*self.config_dictionary['num_events']/pl_sum
+            a0 = chi*a0
+            a1 = self.delta_q*a0
+            err = np.fabs(1.-chi)
+            tries += 1
+            
+        if tries >= max_tries:
+            self.logger.warning("Power-law constrainer reached max # of tries, exiting with error = %.4f"%(err))
+            
+        self.config_dictionary['amp0'] = a0
+        self.config_dictionary['amp1'] = a1
+        self.config_dictionary['amp_array'] = h
 
 
-    def power_law_dist(self,x):
+    def _power_law_dist(self,x,a0,a1,alpha):
         """map uniform variable x to power law distributed variable p for given bounds and index"""
         
-        return ((self.config_dictionary['amp1']**(self.config_dictionary['alpha'] + 1) - self.config_dictionary['amp0']**(self.config_dictionary['alpha']+1))*x + self.config_dictionary['amp0']**(self.config_dictionary['alpha']+1))**(1/(self.config_dictionary['alpha'] + 1))
+        return ((a1**(alpha + 1.) - a0**(alpha + 1.))*x + a0**(alpha + 1.))**(1./(alpha + 1.))
         
