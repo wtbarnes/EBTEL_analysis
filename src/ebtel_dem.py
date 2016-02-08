dea367e26a680ffb5aaa713dfef3e298bb129a37
Will Barnes <wtb2@rice.edu>
2016-02-08 11:52:24 -0600

    fixing outdated reference to time

--- src/ebtel_dem.py
+++ src/ebtel_dem.py
@@ -58,7 +58,7 @@ class DEMProcess(object):
                     continue
                     
             #Estimate percentage of files read
-            self.logger.info("Tn = %d s finished, Estimated total # of events simulated: %.2f %%"%(Tn[i], counter*int(np.ceil(tmp[-1]['t'][-1]/(self.tpulse+Tn[i])))))
+            self.logger.info("Tn = %d s finished, Estimated total # of events simulated: %.2f %%"%(Tn[i], counter*int(np.ceil(t[-1]/(self.tpulse+Tn[i])))))
             
             #append to tope level list
             self.em.append(tmp)
