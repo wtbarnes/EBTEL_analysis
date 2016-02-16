4fde62d64da25a08ca929ae215be05e73556e80f
Will Barnes <wtb2@rice.edu>
2016-02-16 17:49:36 -0600

    adjusting energy budget for single-fluid runs

--- bin/ebtel_configure_main.py
+++ bin/ebtel_configure_main.py
@@ -38,7 +38,9 @@ else:
 
 #Set heating parameters--configure power-law bounds such that loop is maintained at an equilibrium temperature of T_peak
 tpeak = 4.0e+6 #peak temperature for time-averaged heating rate
-Hn = 4.5e-3#1.0e-6*tpeak**(3.5)/(args.loop_length*1.0e+8)**2 #time-averaged heating rate needed to maintain peak temperature of 4 MK
+Hn = 4.5e-3 #1.0e-6*tpeak**(3.5)/(args.loop_length*1.0e+8)**2 #time-averaged heating rate needed to maintain peak temperature of 4 MK
+if args.species == 'single': #reduce energy input for single-fluid case
+    Hn *= 7.8e-7/1.0e-6
 delta_q = 10.0 #range over which power-law distribution is constructed (typically one decade)
 
 #Configure all static dictionary options
