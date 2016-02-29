50e12c279bdae895ceb4c3d7db9b81f86c2e3345
Will Barnes <wtb2@rice.edu>
2016-02-29 12:49:35 -0600

    adding functionality to print IonPopSolver input and configurations from EBTEL output

---
+++ bin/ebtel_configure_main_ips.py
@@ -0,0 +1,60 @@
+#ebtel_configure_main_ips.py
+
+#Will Barnes
+#29 February 2016
+
+#Import needed modules
+import argparse
+import logging
+import sys
+sys.path.append('../src/')
+from ebtel_configure import Configurer
+
+#Declare parser object
+parser = argparse.ArgumentParser(description='Script that prints configuration files for EBTEL-2fluid runs.')
+#Add arguments to parser
+parser.add_argument("-s","--species",help="Species to which the heating was applied for particular run.")
+parser.add_argument("-as","--amp_switch",help="Switch to decide between power-law and uniform heating.")
+parser.add_argument("-a","--alpha",type=float,help="Spectral index for the power-law distribution used.")
+parser.add_argument("-L","--loop_length",type=float,help="Loop half-length.")
+parser.add_argument("-t","--t_pulse",type=float,help="Width of the heating pulse used for the particular run.")
+parser.add_argument("-S","--solver",help="Solver used to compute solutions.")
+parser.add_argument("--root_dir_ebtel",help="Optional root directory for EBTEL data files")
+parser.add_argument("--root_dir",help="Optional root directory for config files")
+parser.add_argument("--t_wait_scaling",type=float,help="Optional parameter to force scaling between wait time and event amplitude, Q\propto T_N^b; b in Cargill(2014)")
+parser.add_argument("--quiet_logger",type=bool,help="Optional parameter to set logging level to warning.")
+#Declare the parser dictionary
+args = parser.parse_args()
+
+#Check for custom root directory
+if args.root_dir:
+    root_dir = args.root_dir
+else:
+    root_dir = '/data/datadrive2/IonPopSolver_runs'
+if args.root_dir_ebtel:
+    root_dir_ebtel = args.root_dir_ebtel
+else:
+    root_dir_ebtel = '/data/datadrive2/EBTEL_runs'
+#check for wait time scaling exponent
+if  args.t_wait_scaling:
+    t_wait_scaling = args.t_wait_scaling
+else:
+    t_wait_scaling = None
+
+#Configure all static dictionary options
+config_dict = {'solver':args.solver}
+config_dict['t_pulse_half'] = 0.5*args.t_pulse
+config_dict['heat_species'] = args.species
+config_dict['amp_switch'] = args.amp_switch
+config_dict['alpha'] = args.alpha
+config_dict['loop_length'] = args.loop_length
+
+#configure logging
+if args.quiet_logger:
+    logging.basicConfig(stream=sys.stdout,level=logging.WARNING)
+else:
+    logging.basicConfig(stream=sys.stdout,level=logging.DEBUG)
+
+config = Configurer(config_dict,root_dir,build_paths=True,t_wait_q_scaling=t_wait_scaling)
+config.print_ips_input(root_dir_ebtel)
+config.print_ips_config()
--- src/ebtel_configure.py
+++ src/ebtel_configure.py
@@ -12,13 +12,13 @@ import xml.etree.ElementTree as ET
 import xml.dom.minidom as xdm
 
 class Configurer(object):
-    
+
     def __init__(self, config_dictionary, root_dir, mc=None, t_wait_q_scaling = None, build_paths=False, Hn=None, delta_q=None, constraint_tol = 1e-3, **kwargs):
-        """Constructor for Configurer class used to print EBTEL configuration files."""
-        
+        """Constructor for Configurer class used to print EBTEL and IonPopSolver configuration files."""
+
         self.config_dictionary = config_dictionary
         self.root_dir = root_dir
-        self.nmc_list = []               
+        self.nmc_list = []
         #set optional variables
         self.Hn = Hn
         self.delta_q = delta_q
@@ -26,7 +26,7 @@ class Configurer(object):
         self.t_wait_q_scaling = t_wait_q_scaling
         self.tol =  constraint_tol
         #configure logger
-        self.logger = logging.getLogger(type(self).__name__) 
+        self.logger = logging.getLogger(type(self).__name__)
         #Set up paths
         if build_paths:
             self.path_builder()
@@ -36,19 +36,19 @@ class Configurer(object):
 
     def path_builder(self,**kwargs):
         """Build path names needed for printing config files and building data folders; check that needed directories exist and create them if necessary."""
-        
-        gen_path = self.config_dictionary['heat_species']+'_heating_runs'            
+
+        gen_path = self.config_dictionary['heat_species']+'_heating_runs'
         if self.config_dictionary['amp_switch'] == 'uniform':
             gen_path = os.path.join(gen_path,'alpha' + self.config_dictionary['amp_switch'])
         else:
-            gen_path = os.path.join(gen_path,'alpha' + str(np.fabs(self.config_dictionary['alpha']))) 
-            
+            gen_path = os.path.join(gen_path,'alpha' + str(np.fabs(self.config_dictionary['alpha'])))
+
         #check for t_wait-q scaling
         if self.t_wait_q_scaling:
             scaling_suffix = '-b'+str(self.t_wait_q_scaling)
         else:
             scaling_suffix = ''
-        
+
         #set data and config paths
         self.data_path = os.path.join(self.root_dir, gen_path,'data')
         self.config_path = os.path.join(self.root_dir, gen_path,'config')
@@ -57,7 +57,7 @@ class Configurer(object):
 
     def print_xml_config(self,config_file='../config/ebtel_config.xml',**kwargs):
         """Print EBTEL XML configuration file from dictionary of input parameters"""
-            
+
         #create root element
         root = ET.Element('root')
 
@@ -69,45 +69,86 @@ class Configurer(object):
                 element = ET.SubElement(root,key)
                 for i in range(len(self.config_dictionary[key])):
                     self._set_xml_element(element,key+str(i),self.config_dictionary[key][i])
-        
+
         #print to file
         with open(config_file,'w') as f:
             f.write(self._pretty_print_xml(root))
-                
-                
+
+
     def _set_xml_element(self,root,name,val):
         """create element, set tag"""
         element = ET.SubElement(root,name)
         element.text = str(val)
-        
-    
+
+
     def _pretty_print_xml(self,element):
         """nicely formatted printing of XML files"""
         unformatted = ET.tostring(element)
         xdmparse = xdm.parseString(unformatted)
         return xdmparse.toprettyxml(indent="    ")
-        
-        
+
+
+    def print_ips_input(self,ebtel_root_dir,**kwargs):
+        """Reshape EBTEL results to be used as IonPopSolver input"""
+
+        #Rebuild paths to read in EBTEL input
+        data_stripped = self.data_path.replace(self.root_dir,'')
+        config_stripped = self.config_path.replace(self.root_dir,'')
+        if data_stripped[0] == '/':
+            data_stripped = data_stripped[1:]
+        if config_stripped[0] == '/':
+            config_stripped = config_stripped[1:]
+        path_to_ebtel_results = os.path.join(ebtel_root_dir,data_stripped)
+        path_to_ebtel_config = os.path.join(ebtel_root_dir,config_stripped)
+
+        #read in job array config file
+        self.config_array = np.loadtxt(os.path.join(path_to_ebtel_config,'ebtel_L' + str(self.config_dictionary['loop_length']) + '_tpulse' + str(2.0*self.config_dictionary['t_pulse_half']) + '_' + self.config_dictionary['solver'] + '_job_array.conf'))
+
+        n_index = 2
+        if self.config_dictionary['heat_species'] == 'electron' or self.config_dictionary['heat_species'] == 'ion':
+            n_index += 1
+
+        #loop through files, print data as configuration file for IonPopSolver
+        for c in self.config_array:
+            tmp_fn = os.path.join(path_to_ebtel_results,self.fn%(c[0]),self.fn%(c[0])+'_'+int(c[1])+'.txt')
+            self.logger.debug('Reshaping EBTEL results file %s'%(tmp_fn))
+            tmp_data = np.loadtxt(tmp_fn)
+            t,T,n = tmp_data[:,0],tmp_data[:,1],tmp_data[:,n_index]
+            np.savetxt(os.path.join(self.config_path,self.fn%(c[0]),self.fn%(c[0])+'_'+int(c[1])+'.reshape.txt'),np.transpose([t,T,n]),header=str(len(t)),comments='',fmt='%f\t%e\t%e')
+
+
+    def print_ips_config(self,**kwargs):
+        """Print file with IonPopSolver configuration options"""
+
+        f = open(os.path.join(self.config_path,'ebtel_L' + str(self.config_dictionary['loop_length']) + '_tpulse' + str(2.0*self.config_dictionary['t_pulse_half']) + '_' + self.config_dictionary['solver'] + '_job_array.conf'),'w')
+
+        for c in self.config_array:
+            f.write('%s\t'%(os.path.join(self.config_path,self.fn%(c[0]),self.fn%(c[0])+'_'+int(c[1])+'.reshape.txt')))
+            f.write('%s\n'%(os.path.join(self.data_path,self.fn%(c[0]),self.fn%(c[0])+'_'+int(c[1])+'.ips_results.txt')))
+
+        f.close()
+
+
     def print_job_array_config(self,**kwargs):
         """Print run number and associated wait time for each unique job to be run according to mc number"""
-        
+
         try:
             top_list = []
             for i in range(len(self.t_wait_mean)):
                 sub_list = []
-                [sub_list.append([self.t_wait_mean[i],j]) for j in range(self.nmc_list[i])]  
+                [sub_list.append([self.t_wait_mean[i],j]) for j in range(self.nmc_list[i])]
                 top_list.append(sub_list)
-                
+
             top_list_flattened = list(itertools.chain(*top_list))
             np.savetxt(os.path.join(self.config_path,'ebtel_L' + str(self.config_dictionary['loop_length']) + '_tpulse' + str(2.0*self.config_dictionary['t_pulse_half']) + '_' + self.config_dictionary['solver'] + '_job_array.conf'),top_list_flattened,fmt='%d')
-            
+
         except AttributeError:
             self.logger.exception("Before printing the job_array.conf file, set up the config path with path_builder and then build the t_wait and nmc_list variables by running self.vary_wait_time.")
 
 
     def vary_wait_time(self,tn_a,tn_b,delta_tn,**kwargs):
         """Print configuration files for varying wait-time between successive heating events"""
-        
+
         #Build wait time list
         self.t_wait_mean = np.arange(tn_a,tn_b+delta_tn,delta_tn)
         #Iterate over wait times
@@ -117,17 +158,17 @@ class Configurer(object):
             #Create directories in data and config if needed
             if not os.path.exists(os.path.join(self.config_path,self.fn%self.t_wait_mean[i])):
                 os.makedirs(os.path.join(self.config_path,self.fn%self.t_wait_mean[i]))
-                
+
             if not os.path.exists(os.path.join(self.data_path,self.fn%self.t_wait_mean[i])):
                 os.makedirs(os.path.join(self.data_path,self.fn%self.t_wait_mean[i]))
-                
+
             #Print config files for each run
             #Check if Monte-Carlo run
             if self.mc:
                 num_runs = self._calc_nmc()
             else:
                 num_runs = 1
-                
+
             self.nmc_list.append(num_runs)
             #Iterate over runs
             for j in range(num_runs):
@@ -139,17 +180,17 @@ class Configurer(object):
                 self.config_dictionary['output_file'] = os.path.join(self.data_path, self.fn%self.t_wait_mean[i], self.fn%self.t_wait_mean[i]+'_'+str(j))
                 #print configuration files
                 self.print_xml_config(config_file=os.path.join(self.config_path, self.fn%self.t_wait_mean[i], self.fn%self.t_wait_mean[i] + '_' + str(j) + '.xml'))
-                
-                
+
+
     def _calc_nmc(self,**kwargs):
         """Calculate number of runs needed to maintain sufficiently large number of heating events"""
-        
+
         return int(np.ceil(self.mc/self.config_dictionary['num_events']))
 
 
     def time_arrays(self,ti,**kwargs):
         """Create start time and end time arrays"""
-        
+
         #preallocate space for start, end time arrays
         self.config_dictionary['start_time_array'], self.config_dictionary['end_time_array'] = np.empty([self.config_dictionary['num_events']]), np.empty([self.config_dictionary['num_events']])
         #calculate coefficient (xi^(1/b))
@@ -170,12 +211,12 @@ class Configurer(object):
                 t_wait_sum = t_wait_sum + t_wait_temp
             else:
                 #increment start time sum
-                t_wait_sum = t_wait_sum + ti    
-                
-                
+                t_wait_sum = t_wait_sum + ti
+
+
     def amp_arrays(self,ti,**kwargs):
         """Configure heating rate amplitudes"""
-        
+
         #calculate coefficient to convert from energy to heating rate
         if self.config_dictionary['heating_shape'] == 'triangle':
             shape_coeff = 2.0/(2.0*self.config_dictionary['t_pulse_half'])
@@ -185,7 +226,7 @@ class Configurer(object):
             shape_coeff = 1.0/(self.config_dictionary['t_pulse_half']*np.sqrt(2.0*np.pi))
         else:
             raise ValueError("Unrecognized heating_shape option. Cannot set heating bounds.")
-            
+
         #calculate uniform heating amplitude
         self.config_dictionary['h_nano'] = self.Hn*self.config_dictionary['total_time']/(self.config_dictionary['num_events'])*shape_coeff
         #dummy power-law bounds
@@ -193,14 +234,14 @@ class Configurer(object):
         if self.config_dictionary['amp_switch'] == 'power_law':
             self.logger.warning("Setting dummy values for power-law bounds.")
         #configure arrays of heating amplitudes from power-law distribution
-        if self.config_dictionary['amp_switch'] == 'file':    
+        if self.config_dictionary['amp_switch'] == 'file':
             np.random.seed()
             self._constrain_distribution(ti)
-            
-            
+
+
     def _constrain_distribution(self,ti,**kwargs):
         """Choose events from power-law distribution such that total desired energy input is conserved."""
-        
+
         #set parameters
         max_tries = 2000
         tries = 0
@@ -223,12 +264,12 @@ class Configurer(object):
                 best = [a0,a1,h]
                 best_err = err
             tries += 1
-        
+
         self.logger.debug("chi = %f, # of tries = %d, error = %f"%(chi,tries,err))
 
         if tries >= max_tries:
             self.logger.warning("Power-law constrainer reached max # of tries, using best guess with error = %f"%best_err)
-            
+
         self.config_dictionary['amp0'] = best[0]
         self.config_dictionary['amp1'] = best[1]
         self.config_dictionary['amp_array'] = best[2]
@@ -236,6 +277,5 @@ class Configurer(object):
 
     def _power_law_dist(self,x,a0,a1,alpha):
         """map uniform variable x to power law distributed variable p for given bounds and index"""
-        
+
         return ((a1**(alpha + 1.) - a0**(alpha + 1.))*x + a0**(alpha + 1.))**(1./(alpha + 1.))
-        
