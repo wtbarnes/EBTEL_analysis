3fdeee290e1c81a960b42a8e8a2a705ce0e5ff06
Will Barnes <wtb2@rice.edu>
2016-06-26 22:45:48 -0500

    freedman and scotts bin widths do not take disp argument

--- src/ebtel_plot_em.py
+++ src/ebtel_plot_em.py
@@ -478,9 +478,9 @@ class EMHistoBuilder(object):
         """Use astroML routines to choose bin edges"""
         
         if bin_tool == 'freedman':
-            dx,bins = density_estimation.freedman_bin_width(hist,return_bins=True, disp=False)
+            dx,bins = density_estimation.freedman_bin_width(hist,return_bins=True)
         elif bin_tool == 'scotts':
-            dx,bins = density_estimation.scotts_bin_width(hist,return_bins=True, disp=False)
+            dx,bins = density_estimation.scotts_bin_width(hist,return_bins=True)
         elif bin_tool == 'knuth':
             dx,bins = density_estimation.knuth_bin_width(hist,return_bins=True, disp=False)
         else:
